bcdaf6bed5ac7c8e5775d36ccc46532ef4fa37d4
tan hung <hung.tt2273@sinhvien.hoasen.edu.vn>
2022-08-18 17:01:13 +0700

    UI information

--- chessman-client/src/app/app-routing.module.ts
+++ chessman-client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'info', loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule) }, 
+{ path: 'info', loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule) },
+{ path: 'game', loadChildren: () => import('./pages/game/game.module').then(m => m.GameModule) },
+{ path: 'lobby', loadChildren: () => import('./pages/lobby/lobby.module').then(m => m.LobbyModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
