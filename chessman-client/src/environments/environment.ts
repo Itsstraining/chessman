4ac4927babffe5b2ad7b69ec007db741df27d0e5
unknown <hsu.tranhuynhduc@gmail.com>
2022-08-20 10:22:42 +0700

    add firebase, fix login page

--- chessman-client/src/environments/environment.ts
+++ chessman-client/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'xiangqi-6c451',
+    appId: '1:359068702954:web:54fa51f4c984dfa7df1f93',
+    storageBucket: 'xiangqi-6c451.appspot.com',
+    apiKey: 'AIzaSyDwhzJQPjuJzXtoh01k5x2Foj6LDs8P1Yg',
+    authDomain: 'xiangqi-6c451.firebaseapp.com',
+    messagingSenderId: '359068702954',
+    measurementId: 'G-Y25D96KBQN',
+  },
   production: false
 };
 
